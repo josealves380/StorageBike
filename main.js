a858c80028bebe96fa2f0fc8f468a2c4dd2459e0
Jose <netopereira464@gmail.com>
2022-06-05 02:29:21 -0300

    Adicionando cards

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function onScroll() {
-  if(scrollY > 0) {
+  if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
@@ -10,6 +10,19 @@ function openMenu() {
   document.body.classList.add('menu-expanded')
 }
 function closeMenu() {
-  document.body.classList.remove('menu-expanded')
+  document.body.classList.remove(
+    'menu-expanded')
 }
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+#home, 
+#home img, 
+#home stats, 
+#services,
+#services header,
+#services .card`);
 
